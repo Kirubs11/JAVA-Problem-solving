5ade6a5a227791afe9fb1ceaacc318379a81c2d1
Kirubs11 <c.kirubakaran1123@gamil.com>
2024-07-19 14:03:47 +0530

    update

--- Double link/palindrome/main.java
+++ Double link/palindrome/main.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.*;
 class Node
 {
@@ -72,79 +71,4 @@ public class Main
         }
         List.pal();
     }
-=======
-import java.util.*;
-class Node
-{
-    int data;
-    Node next;
-    Node prev;
-    Node(int data)
-    {
-        this.data = data;
-        this.next = null;
-        this.prev = null;
-    }
-}
-class DoublyLinkedList
-{
-    Node head;
-    Node tail;
-    void create_List(int data)
-    {
-        Node Newnode = new Node(data);
-        if(head == null)
-        {
-            head = Newnode;
-            tail=Newnode;
-        }
-        else 
-        {
-          tail.next=Newnode;
-          Newnode.prev=tail;
-          tail=Newnode;
-        }
-    }
-    void pal()
-    {
-      Node left=head;
-      Node rig=tail;
-      int count = 0;
-      while(left!=null && rig!=null)
-      {
-        if(left.data == rig.data)
-        {
-          left=left.next;
-          rig=rig.prev;
-        }
-        else
-        {
-         count +=1;
-         break;
-        }
-       }
-      if(count==1) System.out.print("Not a Palindrome");
-      else System.out.print("Palindrome");
-      
-    }   
-}
-public class Main
-{
-    public static void main(String[] args) 
-    {
-        Scanner input = new Scanner(System.in);
-        DoublyLinkedList List = new DoublyLinkedList();
-        int num=0;
-        while(num!=-1)
-        {
-            num = input.nextInt();
-            if(num == -1)
-            {
-                break;
-            }
-            List.create_List(num);
-        }
-        List.pal();
-    }
->>>>>>> 9edbf62f2646a9baf96767fdf49da4375fe72437
 }
--- Double link/reverse/Main.java
+++ Double link/reverse/Main.java
@@ -1,62 +1,4 @@
-<<<<<<< HEAD
-import java.util.*;
 
-class Node {
-  int data;
-  Node next;
-  Node prev;
-
-  Node(int data) {
-    this.data = data;
-    this.next = null;
-    this.prev = null;
-  }
-}
-
-class doubl {
-  Node Head;
-  Node Tail;
-
-  doubl() {
-    Head = null;
-    Tail = null;
-  }
-
-  public void insert(int data) {
-    Node NewNode = new Node(data);
-    if (Head == null) {
-      Head = NewNode;
-      Tail = NewNode;
-    } else {
-      Tail.next = NewNode;
-      NewNode.prev = Tail;
-      Tail = NewNode;
-    }
-  }
-
-  public void reverse() {
-    Node temp = Tail;
-    while (temp != null) {
-      System.out.printf("%d ", temp.data);
-      temp = temp.prev;
-    }
-  }
-}
-
-class Main {
-  public static void main(String[] args) {
-    Scanner ob = new Scanner(System.in);
-    doubl db = new doubl();
-    int val = 0;
-    while (val != -1) {
-      val = ob.nextInt();
-      if (val == -1)
-        break;
-      db.insert(val);
-    }
-    db.reverse();
-  }
-=======
 import java.util.*;
 class node{
   int data;
@@ -118,5 +60,4 @@ class Main{
     }
     db.reverse();
   }
->>>>>>> 9edbf62f2646a9baf96767fdf49da4375fe72437
 }
