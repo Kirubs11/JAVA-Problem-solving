3687c8a1686ed74ed1c73ce6f153051da7877f4f
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-20 10:17:08 +0530

    basic codes

---
+++ simpleproblems/Odd_even.java
@@ -0,0 +1,18 @@
+// Given a number, check whether it is "Odd" or "Even"
+
+import java.util.*;
+class Odd_even{
+  public static void main(String[]args)
+  {
+    Scanner ob=new Scanner(System.in);
+    int a=ob.nextInt();
+    if(a%2==0)
+    {
+      System.out.printf("EVEN");
+    }
+    else
+    {
+      System.out.printf("ODD");
+    }
+  }
+}
