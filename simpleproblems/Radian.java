904ae270bb7805340e7f9d448be568a77ed81b29
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-21 13:46:28 +0530

    new

---
+++ simpleproblems/Radian.java
@@ -0,0 +1,13 @@
+// Given Degree as input,Convert it into Radian using the formulae  Radian = degree * ( M_PI / 180.0 )) (Use M_PI function for pi value)
+
+import java.util.*;
+class Radian{
+  public static void main(String[]args)
+  {
+    double d=3.141414;
+    Scanner ob=new Scanner(System.in);
+    double a=ob.nextDouble();
+    double b=a*(d/180.0);
+    System.out.printf("%.2f",b);
+  }
+}
