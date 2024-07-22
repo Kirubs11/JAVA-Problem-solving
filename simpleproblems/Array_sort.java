dc2482e35158b622d90bcd53770745f9890db65f
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-22 12:52:03 +0530

    new

---
+++ simpleproblems/Array_sort.java
@@ -0,0 +1,21 @@
+// Given a list of elements , arrange the elements in ascending order.
+
+import java.util.*;
+class Array_sort
+{
+  public static void main(String[]args)
+  {
+    Scanner ob=new Scanner(System.in);
+    int n=ob.nextInt();
+    long[] arr=new long[n];
+    for(int i=0;i<n;i++)
+    {
+      arr[i]=ob.nextLong();
+    }
+    Arrays.sort(arr);
+    for(int i=0;i<n;i++)
+    {
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
