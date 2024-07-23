eb294164ec11ac948c02c47b68bd46169fe835f0
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-23 10:14:37 +0530

    today's code

---
+++ simpleproblems/Array_search.java
@@ -0,0 +1,25 @@
+/*Complete the function Search_element(), which receives number of elements,array and search element as input.
+Function should find and return 1 if search element is found in the input array, return 0 if not found.*/
+
+
+import java.util.*;
+class Array_search{
+  public static void main(String[]args)
+  {
+    Scanner ob=new Scanner(System.in);
+    int[] a={1,2,3,4,5,6,7,8,9,10};
+    int key=ob.nextInt();
+    int i=0,b=0;
+    for(i=0;i<n;i++)
+    {
+      if(a[i]==key){ b=key;
+      break;}
+      
+    }
+    if(b==key)
+    {
+      System.out.print("Found");
+    }
+    else System.out.print("Not found");
+  }
+}
