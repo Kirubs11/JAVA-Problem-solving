75f4b999786ab1312510b2ed79e22bce3391a87f
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-23 10:14:04 +0530

    today's code

---
+++ simpleproblems/Absolute_value.java
@@ -0,0 +1,12 @@
+// Given an integer input, print the Absolute value of the inputnumber
+
+import java.util.*;
+class Absolute_value{
+  public static void main(String[]args)
+  {
+    Scanner ob=new Scanner(System.in);
+    int a=ob.nextInt();
+    
+    System.out.print(Math.abs(a));
+  }
+}
