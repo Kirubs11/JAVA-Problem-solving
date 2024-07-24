45084a2b2bd2ce8c00070a2c131095b43bd5b80c
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-24 10:02:19 +0530

    today's code

---
+++ simpleproblems/FirstNonRepeatingElement.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class FirstNonRepeatingElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the no.of elements : ");
+        int n = sc.nextInt();
+        System.out.println("The array elements are : ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int result = findFirstNonRepeatingElement(arr, n);
+        System.out.println("The  first non repeated element is : ");
+        System.out.println(result);
+    }
+
+    private static int findFirstNonRepeatingElement(int[] arr, int n) {
+        int[] count = new int[n];
+        for (int i = 0; i < n; i++) {
+            count[i] = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j && arr[i] == arr[j]) {
+                    count[i]++;
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (count[i] == 1) {
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
