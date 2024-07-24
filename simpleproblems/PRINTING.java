c014ef009957e495627ce0fc293b9ada3e6b7b89
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-07-24 10:18:29 +0530

    today's code

---
+++ simpleproblems/PRINTING.java
@@ -0,0 +1,8 @@
+// Print the following output : Success is when your "signature" becomes "autograph"
+
+class PRINTING{
+  public static void main(String[] args)
+  {
+    System.out.println("Success is when your \\\\"signature\\\\" becomes \\\\"autograph\\\\"");
+  }
+}
