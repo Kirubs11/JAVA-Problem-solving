df6cd73da4a5d3f4c8d1d256cf2b0d6cd4f06f32
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-05 17:18:51 +0530

    today's code

---
+++ simpleproblems/Rigth_rotation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Rigth_rotation{
+  public static void main(String[] args)
+  {
+    Scanner ob=new Scanner(System.in);
+    int n=ob.nextInt();
+    int[] a=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      a[i]=ob.nextInt();
+    }
+    int r=ob.nextInt();
+    int[] res=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      res[i]=a[(i-r+n)%n];
+    }
+    for(int i=0;i<n;i++)
+    {
+      System.out.printf("%d ",res[i]);
+    }
+  }
+}
