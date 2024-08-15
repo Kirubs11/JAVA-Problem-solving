069537273d2ad3f4cfcba9371ac09a997b08fdbc
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-15 10:20:18 +0530

    today's code

---
+++ collection framework/Cpy_arrList.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Cpy_arrList {
+    public static void main(String[] args)
+    {
+        Scanner ob = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        ArrayList<Integer> list2 = new ArrayList<Integer>();
+        int count=0;
+        while(true)
+        {
+            int val=ob.nextInt();
+            if(val==-1) break;
+            list.add(val);
+            count++;
+        }
+        System.out.println("original list: " + list);
+        for(int i=0;i<count;i++)
+        {
+            list2.add(list.get(i));
+        }
+        System.out.println("list2: " + list2);
+    }
+}
