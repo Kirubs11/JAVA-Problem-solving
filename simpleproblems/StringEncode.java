f7688b45bac03e6e2f97254609ca0e748027bd4f
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-17 10:08:00 +0530

    today's code

---
+++ simpleproblems/StringEncode.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class StringEncode
+{
+  public static void main(String[] args)
+  {
+    String en="";
+    Scanner ob=new Scanner(System.in);
+    String a=ob.nextLine();
+    int n=ob.nextInt();
+    for(int i=0;i<a.length();i++)
+    {
+      char c=a.charAt(i);
+      if(c==' ') c-=n;
+      c+=n;
+      en+=c;
+    }
+    System.out.printf("The encrypted text is : %s",en);
+  }
+}
