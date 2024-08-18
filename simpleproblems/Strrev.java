8c8d96861279c63089d4dbb1f1a64ff55b259833
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-18 07:35:17 +0530

    today's code

---
+++ simpleproblems/Strrev.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Strrev {
+    public static void main(String args[])
+    {
+        Scanner ob=new Scanner(System.in);
+        String s=ob.nextLine();
+        String ns="";
+        String w="";
+        int i=0;
+        while(i<=s.length())
+        {
+            char c=s.charAt(i);
+            if(c!=' ' || c!=null)
+            {
+                w=c+w;
+                i++;
+            }
+            else
+            {
+                ns = ns + w + " ";
+                i++;
+            }
+        }
+        System.out.print(ns);
+    }
+}
