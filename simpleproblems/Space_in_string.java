e5580d3925c66201cf605bc9e2116d36b67e71a8
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-24 07:03:34 +0530

    aug_24

---
+++ simpleproblems/Space_in_string.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class Space_in_string
+{
+    public static void main(String[] args)
+    {
+        Scanner ob=new Scanner(System.in);
+        String s=ob.next();
+        int i=0;
+        while(i<s.length())
+        {
+            char c=s.charAt(i);
+            if(c>='A' && c<='Z')
+            {
+                if(i!=0)
+                {
+                    System.out.print(" "+(char)(c+32));
+                }
+                else
+                {
+                    System.out.print((char)(c+32));
+                }
+            }
+            else
+            {
+                System.out.print(c);
+            }
+            i++;
+        }
+    }
+}
+
+/* Input is 
+    HeyYou.
+
+   Output is
+    hey you.
+*/
