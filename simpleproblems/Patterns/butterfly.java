9c931803c39bfa4b668179aa3d95f34641894246
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-26 06:39:56 +0530

    aug_26

---
+++ simpleproblems/Patterns/butterfly.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class butterfly{
+    public static void main(String[]args)
+    {
+        Scanner ob=new Scanner(System.in);
+        int n=ob.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int k=1;k<=(n-i)*2;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=n;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int k=1;k<=(n-i)*2;k++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
