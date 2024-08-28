f3e4b5f412a20d241b847d982ac124ad2d1c4e38
Kirubakaran C <117499863+Kirubs11@users.noreply.github.com>
2024-08-28 06:47:43 +0530

    aug_28

---
+++ simpleproblems/Array_problems/MATRIX_multiply.java
@@ -0,0 +1,45 @@
+import java.util.*;
+class MATRIX_multiply{
+  public static void main(String[]args)
+  {
+    Scanner ob=new Scanner(System.in);
+    int r=ob.nextInt();
+    int c=ob.nextInt();
+    int[][] ar=new int[r][c];
+    for(int i=0;i<r;i++)
+    {
+      for(int j=0;j<c;j++)
+      {
+        ar[i][j]=ob.nextInt();
+      }
+    }
+    int[][] ar1=new int[r][c];
+    for(int i=0;i<c;i++)
+    {
+      for(int j=0;j<r;j++)
+      {
+        ar1[i][j]=ar[j][i];
+      }
+    }
+    int[][] res=new int[r][c];
+    for(int k=0;k<c;k++)
+    {
+      for(int i=0;i<r;i++)
+      {
+        for(int j=0;j<c;j++)
+        {
+          res[i][k]+=ar[k][j]*ar1[j][i];
+        }
+      }
+    }
+    System.out.println("Result:");
+    for(int i=0;i<r;i++)
+    {
+      for(int j=0;j<c;j++)
+      {
+        System.out.print(res[i][j]+" ");
+      }
+      System.out.println();
+    }
+  }
+}
